cbf4beff298368bb952a128aa135d2aac8386733
sruba mohapatra <sruba209239@gmail.com>
2021-02-15 01:34:50 +0530

    added basic routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
@@ -12,7 +13,8 @@ import { SecondComponent } from './second/second.component';
     SecondComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

c16fdbe1ee109fa00158e380fc3b211b67c81d67
sruba mohapatra <sruba209239@gmail.com>
2021-04-04 18:30:54 +0530

    added page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SecondComponent } from './second/second.component';
 
 const routes: Routes = [
   { path: 'first-component', component: FirstComponent },
   { path: 'second-component', component: SecondComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
+    SecondComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
